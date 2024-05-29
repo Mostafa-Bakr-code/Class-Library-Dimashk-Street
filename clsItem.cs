7f53b6832d9b67608384273bb060c8c28459fcdb
Mustafa Bakr <mustafa.sabea.amazon@gmail.com>
2024-05-29 16:12:15 +0300

    add main files

---
+++ clsItem.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassItemLibrary
+{
+    public class clsItem 
+    {   
+        
+   
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public bool MarkForDelete = false;
+
+        public clsItem(string Name, float Price)
+        {
+            this.Name = Name;
+            this.Price = Price;
+        }
+
+ 
+        public void addItemToList()
+        {
+           
+            List<clsItem> myItems = LoadDataFromFileToObjList();
+            myItems.Add(this);
+            LoadDataFromObjListToFile(myItems);
+
+        }
+
+        public void updateItem()
+        {
+            List<clsItem> myItems = LoadDataFromFileToObjList();
+
+            for(int i = 0; i < myItems.Count; i++)
+            {
+                
+                if (myItems[i].Name == Name)
+                {
+                    myItems[i] = this;
+                    break;
+                }
+            }
+
+            LoadDataFromObjListToFile(myItems);
+        }
+
+        public void deleteItem()
+        {
+            List<clsItem> myItems = LoadDataFromFileToObjList();
+
+            for (int i = 0; i < myItems.Count; i++)
+            {
+
+                if (myItems[i].Name == Name)
+                {
+                    myItems[i].MarkForDelete = true;
+                    break;
+                }
+            }
+
+            LoadDataFromObjListToFile(myItems);
+
+        }
+
+        public static clsItem findItem(string itemName)
+        {
+            List<clsItem> myItems = LoadDataFromFileToObjList();
+
+            for (int i = 0; i < myItems.Count; i++)
+            {
+                if (myItems[i].Name == itemName)
+                {
+                    return myItems[i];
+                    
+                }
+            }
+
+            return null;
+        }
+
+        public static bool isItemExist(string itemName)
+        {
+            if(findItem(itemName) != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //_________________________________________________________________________
+        // Files
+
+        public static clsItem ConvertLineToItemObject(string line, string separator = "#//#")
+        {
+            string[] itemFields = line.Split(new string[] { separator }, StringSplitOptions.None);
+       
+            return new clsItem(itemFields[0], Convert.ToSingle (itemFields[1]));
+        }
+
+        public static List<clsItem> LoadDataFromFileToObjList()
+        {
+            List<clsItem> itemRecordList = new List<clsItem>();
+            using (StreamReader reader = new StreamReader("Items.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Check if the line is not empty or just whitespace
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        itemRecordList.Add(ConvertLineToItemObject(line));
+                    }
+                }
+            }
+            return itemRecordList;
+        }
+
+        public static string ConvertItemObjectToLine(clsItem item, string separator = "#//#")
+        {
+
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "Item cannot be null");
+            }
+
+            
+            if (string.IsNullOrWhiteSpace(item.Name))
+            {
+                throw new ArgumentException("Item name cannot be null or empty", nameof(item.Name));
+            }
+
+           
+            if (item.Price < 0)
+            {
+                throw new ArgumentException("Item price cannot be negative", nameof(item.Price));
+            }
+
+            return $"{item.Name}{separator}{item.Price}";
+        }
+
+        public static void LoadDataFromObjListToFile(List<clsItem> itemRecordList)
+        {
+            using (StreamWriter writer = new StreamWriter("Items.txt"))
+            {
+                foreach (clsItem item in itemRecordList)
+                {
+                    if (!item.MarkForDelete)
+                    {
+                        writer.WriteLine(ConvertItemObjectToLine(item));
+                    }
+                  
+                }
+            }
+        }
+
+        //_____________________________________________________________________________
+
+
+    }
+
+
+
+
+}
---
+++ clsOrder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassItemLibrary
+{
+    public class clsOrder
+    {
+        public  List<clsItem> orderItems = new List<clsItem>();
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public float Total {  get; set; }
+        public int orderNumber { get; set; }
+        public clsOrder(string date, string time, float total,int OrderNumber ,List<clsItem> itemsList)
+        {
+            //DateTime now = DateTime.Now;
+            //Date = now.ToString("yyyy-MM-dd"); 
+            //Time = now.ToString("HH:mm:ss");
+            Date = date;
+            Time = time;
+            Total = total;
+            orderNumber = OrderNumber;
+            orderItems = itemsList;
+
+        }
+
+        //_________________________________________________________________________
+        // Files
+
+        public static String[] ConvertLineToItemsArr(string line, string separator = "-")
+        {
+            // line is burger/50-hotdog/60
+
+            string[] ArrItems = line.Split(new string[] { separator }, StringSplitOptions.None);
+
+            return ArrItems;
+
+            // return [burger/50, hotdog/60]
+        }
+
+        public static List<clsItem> ConvertArrItemsToclsItems(string[] ArrItems, string separator = "/")
+        {
+            List<clsItem> ItemsList = new List<clsItem>();
+
+            for(int i = 0; i < ArrItems.Length; i++)
+            {
+                string[] orderFields = ArrItems[i].Split(new string[] { separator }, StringSplitOptions.None);
+
+                // now i have a array string carrying [burger, 50]
+                
+                clsItem item = new clsItem(orderFields[0], Convert.ToSingle (orderFields[1]));
+                ItemsList.Add(item);
+            }
+
+            return ItemsList;
+        }
+
+        public static clsOrder ConvertLineToOrderObject(string line, string separator = "#//#")
+        {
+
+            string[] orderFields = line.Split(new string[] { separator }, StringSplitOptions.None);
+
+
+
+            return new clsOrder(orderFields[0], orderFields[1] ,Convert.ToSingle(orderFields[2]),
+                int.Parse(orderFields[3]), ConvertArrItemsToclsItems(ConvertLineToItemsArr(orderFields[4])) );
+        }
+
+        public static List<clsOrder> LoadDataFromFileToObjList()
+
+
+        {
+            List<clsOrder> itemRecordList = new List<clsOrder>();
+            using (StreamReader reader = new StreamReader("OrderItems.txt"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        itemRecordList.Add(ConvertLineToOrderObject(line));
+                    }
+                }
+            }
+            return itemRecordList;
+        }
+
+        //_______________________________________________________________________
+
+        public static string ConvertOrderObjectToLine(clsOrder order, string separator = "#//#")
+        {
+
+            string items = string.Join("-", order.orderItems.Select(item => $"{item.Name}/{item.Price}"));
+            return $"{order.Date}{separator}{order.Time}{separator}{order.Total}{separator}{order.orderNumber}{separator}{items}";
+        }
+
+        public static void LoadDataFromObrderListToFile(List<clsOrder> OrderRecordList)
+        {
+            using (StreamWriter writer = new StreamWriter("OrderItems.txt"))
+            {
+                foreach (clsOrder order in OrderRecordList)
+                {
+                        writer.WriteLine(ConvertOrderObjectToLine(order));
+                }
+            }
+        }
+
+        //_____________________________________________________________________________
+
+    }
+}
